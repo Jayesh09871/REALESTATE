1f8e1f04af8be23f79d5124074081322f95ba011
tohit1009 <tohit0333@gmail.com>
2025-03-08 09:56:58 +0530

    Appointments

--- admin/src/pages/Appointments.jsx
+++ admin/src/pages/Appointments.jsx
@@ -184,11 +184,11 @@ const Appointments = () => {
           </div>
         </div>
 
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden ">
           <div className="overflow-x-auto">
             <table className="w-full">
-              <thead className="bg-gray-50">
-                <tr>
+              <thead className="bg-gray-50 shadow-lg rounded-lg ">
+                <tr className="bg-orange-100 px-6 py-4">
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Property
                   </th>
